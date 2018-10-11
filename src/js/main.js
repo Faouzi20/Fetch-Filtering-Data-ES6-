5e6b70524cf265909626e6a6e795dc2c23fdf233
Faouzi20 <faouzi.edrissi@gmail.com>
2018-10-11 09:18:01 +0100

    Dans cet commit, nous avons lu et récupéré les données á l'utilisateur

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+import { data } from '../js/data';
+
+let placeHolder = document.querySelector(".js-placeholder");
+
 // When the user scrolls down 20px from the top of the Page, show the button
 function onscroll(){
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -14,7 +18,30 @@ function topPage(){
 }
 
 
+function mapData(listData){
+    return listData.map(key => {
+        return `<div class="card">
+                    <a href="${key.link}"><img src="${key.pictures.sizes[0].link}" alt="profile" class="card__img"></a>
+                    <div class="card__content">
+                        <h2 class="card__title"><a href="${key.link}">${key.name}</a></h2>
+                        <p class="card__description">${key.description && key.description.substr(0, 150)}${key.description && key.description.length > 150 ? '...' : ''}</p>
+                        <ul class="card__metadata">
+                            <li><i class="fas fa-play"></i>  ${key.stats.plays}</li>
+                            <li><i class="fas fa-comment"></i>  ${key.metadata.connections.comments.total}</li>
+                            <li><i class="fas fa-heart"></i>  ${key.metadata.connections.likes.total}</li>
+                        </ul>
+                    </div>
+                </div>`
+    }).join("");
+}
+
+function viewData(){
+    placeHolder.innerHTML = mapData(data);
+}
+
+
 function __init(){
+    viewData();
     topPage();
 }
 window.onscroll = onscroll;
